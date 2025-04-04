5c880fc3105451f797cfd84556d0aa89e045522e
nicolas <nicolassauter97@gmail.com>
2025-04-04 09:15:39 +0200

    Update image classification model and add example image for pet classification

--- week7/gradio-oxford-pet-app/app.py
+++ week7/gradio-oxford-pet-app/app.py
@@ -2,7 +2,7 @@ import gradio as gr
 from transformers import pipeline
 
 # Load models
-vit_classifier = pipeline("image-classification", model="kuhs/vit-base-oxford-iiit-pets")
+vit_classifier = pipeline("image-classification", model="n1kooo/vit-base-oxford-iiit-pets")
 clip_detector = pipeline(model="openai/clip-vit-large-patch14", task="zero-shot-image-classification")
 
 labels_oxford_pets = [
@@ -18,12 +18,13 @@ def classify_pet(image):
     vit_output = {result['label']: result['score'] for result in vit_results}
     
     clip_results = clip_detector(image, candidate_labels=labels_oxford_pets)
-    clip_output = {result['label']: result['score'] for result in clip_results['labels']}
+    clip_output = {result['label']: result['score'] for result in clip_results}
     
     return {"ViT Classification": vit_output, "CLIP Zero-Shot Classification": clip_output}
 
 example_images = [
     ["example_images/dog1.jpeg"],
+    ["example_images/leonberger.jpeg"]
     ["example_images/dog2.jpeg"],
     ["example_images/snow_leopard.jpeg"],
     ["example_images/cat.jpg"]
